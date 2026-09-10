356ff4f4ed810a115680abea6a73933e4fd03710
Anurag Deshpande <adeshpande@idfbins.com>
2026-09-10 17:34:47 -0600

    fix(cache): bump service worker cache version to v35

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,6 +1,6 @@
 // PrintHive Service Worker
-const CACHE_NAME = 'printhive-v34';
-const STATIC_CACHE = 'printhive-static-v33';
+const CACHE_NAME = 'printhive-v35';
+const STATIC_CACHE = 'printhive-static-v35';
 
 // Static assets to cache on install
 const STATIC_ASSETS = [
